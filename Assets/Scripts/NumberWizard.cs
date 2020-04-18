84e0e0fcccd36c1a18769da7fffad8dd8aa95bcd
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-04-18 13:42:26 +0100

    Methods higher and lower

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField]
+    int max;
+
+    [SerializeField]
+    int min;
+
+    int currentGuess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    public void OnPressHigher()
+    {
+        min = currentGuess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = currentGuess;
+        NextGuess();
+    }
+
+    void StartGame()
+    {
+        max = max + 1;
+
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        currentGuess =  (max + min) / 2;
+    }
+
+}
